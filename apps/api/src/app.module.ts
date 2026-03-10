0ccefaae13cca247d63f764f03967057296799be
Francis Albores <mrthemetribe@gmail.com>
2026-03-10 22:22:21 +0800

    feat(api): scaffold core modules for users, notes, and collaboration

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { NotesModule } from './notes/notes.module';
+import { CollaborationModule } from './collaboration/collaboration.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, NotesModule, CollaborationModule],
   controllers: [AppController],
   providers: [AppService],
 })
