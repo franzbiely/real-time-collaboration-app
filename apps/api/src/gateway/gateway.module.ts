a41581f4054669d71968e6cc5fb33158f879a04b
Francis Albores <mrthemetribe@gmail.com>
2026-03-10 22:00:49 +0800

    chore: setup turborepo monorepo architecture

--- apps/api/src/gateway/gateway.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { NotesModule } from '../notes/notes.module';
-import { NotesGateway } from './notes.gateway';
-
-@Module({
-  imports: [NotesModule],
-  providers: [NotesGateway],
-})
-export class GatewayModule {}
